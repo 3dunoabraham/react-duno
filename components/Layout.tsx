3e40ae491264d98b05a0f695458014a58886ecbc
Abraham Duno <71507184+3dunoabraham@users.noreply.github.com>
2023-01-17 14:16:14 -0400

    Create Layout.tsx

---
+++ components/Layout.tsx
@@ -0,0 +1,32 @@
+import Head from "next/head"
+import Link from "next/link"
+import { ReactNode } from "react"
+
+export interface ComponentProps {
+    children: ReactNode
+    title?: string
+  }
+
+export default ({
+    children,
+    title = 'Home',
+  }: ComponentProps) => {
+    return (<>
+	<Head>
+		<title>{title ? `${title} | DDCAG` : 'DDCAG'}</title>
+		<meta name="description" content="ServicePad Inventory Management System" />
+		{/* <link rel="icon" href={"/icons/favicon.ico"} /> */}
+		<link rel="icon" href={"/favicon.ico"} />
+	</Head>
+	<div className="flex-1 w-100">
+		{children}    
+	    <div className="pa-2 py-8 flex-center  tx-white footer z-999 block w-100 pos-rel">
+			<div className="tx-lg tx-white  clickble underline">
+				<Link href="/">
+					<a className="pa-2 py-8">Go Home</a>
+				</Link>
+			</div>
+	    </div>
+	</div>
+    </>)
+}
