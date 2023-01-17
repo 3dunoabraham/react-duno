7c94d659d13603a7eb6f14b30b2f0380877018bf
Abraham Duno <71507184+3dunoabraham@users.noreply.github.com>
2023-01-17 14:13:38 -0400

    Delete scripts/helpers directory

--- scripts/helpers/fetchHelper.ts
+++
@@ -1,7 +0,0 @@
-export async function fetchJsonArray(theUrl, propName = "") {
-    let theRequest = await fetch(theUrl);
-    let headerCType = theRequest.headers.get("content-type");
-    let theResult = headerCType && headerCType.includes("application/json") ? await theRequest.json() : []
-    let theParsedResult = propName == "" ? theResult : theResult[propName]
-    return theParsedResult
-}
--- scripts/helpers/mathHelper.ts
+++
@@ -1,4 +0,0 @@
-export const lerp = (start, end, amt) =>
-{
-	return (1-amt)*start+amt*end
-}
