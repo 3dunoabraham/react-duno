e03d798210a45fa27c507bae8bf46252397c61e0
Abraham Duno <71507184+3dunoabraham@users.noreply.github.com>
2023-01-17 14:12:55 -0400

    Create types.ts

---
+++ scripts/types.ts
@@ -0,0 +1,23 @@
+
+export type ILink = {
+    id: number;
+    href: string;
+    src: string;
+    name: string;
+};
+export type IService = {
+    id: number;
+    domain: number;
+    hosting: number;
+    ssl: number;
+    ownership: number;
+    languages: number;
+    seo: number;
+    pages: number;
+    searchbar: number;
+    socialmedia: number;
+    products: number;
+    posts: number;
+    website: number;
+    app: number;
+};
